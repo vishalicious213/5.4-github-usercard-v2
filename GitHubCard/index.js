5ecf65c3f56d6a29bed5ccd737e8468c72b99b47
vishalicious213 <vishalicious213@gmail.com>
2019-10-10 22:40:04 -0400

    made static followers/friends array

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -37,7 +37,7 @@ axios.get("https://api.github.com/users/vishalicious213")
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["BaoPham92", "zeravenyoej", "Lfritze", "mxxt1", "darrenjcarrillo", "kukicako", "Jnmendza", "seanaleid", "MrT3313"];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
